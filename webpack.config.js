bc979778ab5ccc7453f279184882807a4ee7853c
ben-cunningham <bencunningham17@gmail.com>
2018-07-01 15:27:55 -0700

    Update configurations

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+    entry: './assets/javascript/main.js',
+    output: { 
+        path: __dirname +'/dist', 
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /.json$/,
+                loader: 'json'
+            },
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+    }
+};
+
